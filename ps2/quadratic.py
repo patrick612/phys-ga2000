8962e2d79fafa1ad3f070ad69cf4271ea93eda78
patrick612 <plee95612@gmail.com>
2023-09-19 12:18:59 -0400

    .

--- ps2/quadratic.py
+++ ps2/quadratic.py
@@ -13,3 +13,23 @@ def quadratic(a, b, c, method_b = False):
         return x_b_1, x_b_2
     else:
         return x_a_1, x_a_2
+
+#a)
+root1, root2 = quadratic(0.001, 1000, 0.001)
+
+#b)
+root1, root2 = quadratic(0.001, 1000, 0.001, method_b=True)
+
+#c)
+def quadratic_corrected(a, b, c):
+    if len(str((-b - np.sqrt(np.power(b, 2) - 4*a*c)))) >= len(str(2*a)):
+        x1 = (-b + np.sqrt(np.power(b, 2) - 4*a*c))/(2*a)
+    else:
+        x1 = (2*c)/(-b - np.sqrt(np.power(b, 2) - 4*a*c))
+    if len(str((-b + np.sqrt(np.power(b, 2) - 4*a*c)))) >= len(str(2*a)):
+        x2 = (-b - np.sqrt(np.power(b, 2) - 4*a*c))/(2*a)
+    else:
+        x2 = (2 * c) / (-b + np.sqrt(np.power(b, 2) - 4 * a * c))
+    return x1, x2
+
+
